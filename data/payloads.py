f391383ccda511c882966b09d1e50451546d0b99
bexultvn <bexultan2710@gmail.com>
2026-02-01 21:09:52 +0500

    updated payloads

--- data/payloads.py
+++ data/payloads.py
@@ -1,7 +1,19 @@
+import uuid
+
+
 def valid_user_payload():
+    unique_email = f"qa_{uuid.uuid4().hex[:8]}@example.com"
+
     return {
         "name": "Test User",
-        "email": "test_user_qa@example.com",
+        "email": unique_email,
         "gender": "male",
-        "status": "active"
+        "status": "active",
+    }
+
+
+def update_user_payload():
+    return {
+        "name": "Updated User",
+        "status": "inactive",
     }
