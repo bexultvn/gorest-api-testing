0ae7cedb3b90228d1475c1da7a09c7758b271446
bexultvn <bexultan2710@gmail.com>
2026-02-01 21:10:56 +0500

    passed test cases 7-9

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,6 @@
-from data.payloads import valid_user_payload
+from data.payloads import valid_user_payload,update_user_payload
+import requests
+BASE_URL = "https://gorest.co.in/public/v2"
 
 
 #TC-001 — Get users list
@@ -58,3 +60,46 @@ def test_create_user_invalid_email(api):
     payload["email"] = "invalid_email"
     response = api.post("/users",payload)
     assert response.status_code == 422
+
+
+#TC-007
+def test_create_user_without_authorization():
+    payload = valid_user_payload()
+
+    response = requests.post(
+        url=f"{BASE_URL}/users",
+        json=payload,
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    )
+
+    assert response.status_code == 401
+
+
+#TC-008 — Update existing user
+def test_update_existing_user(api):
+    create_resp = api.post("/users",valid_user_payload())
+    assert create_resp.status_code == 201
+
+    user = create_resp.json()
+    user_id = user["id"]
+
+    update_payload = update_user_payload()
+    update_resp = api.put(f"/users/{user["id"]}",update_payload)
+    assert update_resp.status_code == 200
+
+    updated_user = update_resp.json()
+    assert updated_user["id"] == user_id
+    assert updated_user["name"] == update_payload["name"]
+    assert updated_user["status"] == update_payload["status"]
+
+
+#TC-009 — Update non-existing user
+def test_update_non_existing_user(api):
+    non_existing_user_id = 999999999
+    payload = update_user_payload()
+
+    response = api.put(f"/users/{non_existing_user_id}",payload)
+    assert response.status_code == 404
